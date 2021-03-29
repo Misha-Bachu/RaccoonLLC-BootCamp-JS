bd05ac7693a22c3f01327e0598a64cc2fc059a1f
Misha Bachu <m.bachu@emakina.ae>
2021-03-29 23:25:14 +0300

    LSN-004:ADD: homepage

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,15 @@
 const Router = require('express');
-const path = require('path');
+const productHelpers = require('../scripts/helpers/productHelpers');
 
 const router = Router();
 
 router.get('^/$|/home', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../static', 'staticHomePage.html'));
+    let products = productHelpers.getProducts();
+    products = products.slice(0, 3);
+
+    res.render('homepage', {
+        products
+    });
 });
 
 module.exports = router;
---
+++ scripts/helpers/productHelpers.js
@@ -0,0 +1,9 @@
+const ALL_PRODUCTS = require('../../config/products');
+
+function getProducts() {
+    return ALL_PRODUCTS;
+}
+
+module.exports = {
+    getProducts
+};
